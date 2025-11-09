7189d62dad8c51a12935f3fc5125b9b8d489918c
GHONGO <griffinwhongo@gmail.com>
2025-11-09 22:27:33 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,10 @@ const io = new Server(httpServer, {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
